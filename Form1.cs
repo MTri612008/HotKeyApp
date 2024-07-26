9aa9ad4680376a2c2d88806e0bdc204eed3674b2
MTri612008 <m.tri612008@gmail.com>
2024-07-27 01:40:59 +0700

    commit

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace HotKey
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+       
+    }
+}
---
+++ HotKey.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotKey
+{
+    internal class HotKey:Form
+    {
+        #region Var
+        private static IntPtr _hookID = IntPtr.Zero;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WH_KEYBOARD_LL = 13;
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        static Keys previoursKey = Keys.Separator;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams @params = base.CreateParams;
+                @params.ExStyle |= 0x08000000;
+                return @params;
+            }
+        }
+        #endregion
+        public HotKey()
+        {
+            HookKeyboard();
+        }
+        #region DLLImports
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int ncode, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        #endregion
+
+
+        #region funtions
+        static void HookKeyboard()
+        {
+            _hookID = SetHook(_proc);
+            Application.Run(new Form1());
+            UnhookWindowsHookEx(_hookID);
+        }
+
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            {
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+        }
+        public static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                setHotKey(vkCode);
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        #endregion
+
+
+        private static void setHotKey(int vkCode)
+        {
+            if (previoursKey == Keys.LControlKey && (Keys)(vkCode) == Keys.F3)
+            {
+                Process.Start("E:\\memev6\\khi.jpg");
+            }
+            previoursKey = (Keys)vkCode;
+        }
+    }
+}
